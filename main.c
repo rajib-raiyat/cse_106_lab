866d3c1b277c2500e3444e5fbdd77344e24b3025
Komol Kunty Rajib <rajib2@qmail.com.bd>
2021-05-23 12:55:01 +0600

    Fix this code.
        + Time calculation system added.
        + Handshake logic proved.

--- main.c
+++ main.c
@@ -1,24 +1,34 @@
+//  The code and commit(s) are available at this Git repository.
+//  https://github.com/rajib-raiyat/cse_106_lab
+
+//  Import some libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SIZE 1000 // Define a size
 
-int matrix[SIZE][SIZE];
-
 int main() {
-    int i, j, dice, degree = 0, edge = 0;
-    double computational_time_ns;
+    int matrix[SIZE][SIZE]; // Define a 2D matrix.
+    int i, j, dice, degree = 0, edge = 0;  // Declare some integer type variable.
+    double computational_time_ns; // Declare double type variable to calculate program run time.
 
-    for (i = 0; i <= SIZE; i++) {
-        for (j = i; j <= SIZE; j++) {
+    // This program will create different sequence of random numbers on every program run.
+    srand(time(0)); // Use current time as seed for random generator
+
+    // This for loop helps to create a SIZE * SIZE matrix.
+    for (i = 0; i < SIZE; i++) {
+        for (j = i; j < SIZE; j++) {
             dice = rand() % 2;
             matrix[i][j] = dice;
             matrix[j][i] = dice;
         }
     }
 
-    for (i = 0; i <= SIZE; i++) {
-        for (j = 0; j <= SIZE; j++) {
+    clock_t begin = clock(); // Start counting program run time.
+
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
             if (i == j && matrix[i][j] == 1) {
                 degree++;
             }
@@ -28,15 +38,28 @@ int main() {
         }
     }
 
-    for (i = 0; i <= SIZE; i++) {
-        for (j = i; j <= SIZE; j++) {
+    for (i = 0; i < SIZE; i++) {
+        for (j = i; j < SIZE; j++) {
             if (matrix[i][j] == 1) {
                 edge++;
             }
         }
     }
 
+    clock_t end = clock(); // End counting program run time.
+
+    if (degree % (edge * 2) == 0) {
+        printf("Handshake Logic proved.\n");
+    } else {
+        printf("Handshake Logic not proved.\n");
+        exit(0);
+    }
+
     printf("degree = %d\nedges = %d\n", degree, edge);
+
+    computational_time_ns = ((double) (end - begin) / CLOCKS_PER_SEC);
+    printf("Program runtime = %.3lf second or %.2lf nano second.\n", computational_time_ns,
+           computational_time_ns * 1000000000);
 
     return 0;
 }
